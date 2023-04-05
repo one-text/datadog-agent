f611a42e92fe9d52acea9749829ae97e81a88de5
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-04-05 13:09:28 +0200

    make it work

--- pkg/network/protocols/http/shared_libraries.go
+++ pkg/network/protocols/http/shared_libraries.go
@@ -17,6 +17,7 @@ import (
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 	"unsafe"
 
 	"github.com/DataDog/gopsutil/process"
@@ -294,6 +295,39 @@ func (r *soRegistry) Unregister(pid uint32) {
 	delete(r.byPID, pid)
 }
 
+func quircksWaitLib(hostlibPath string) error {
+	// java netty when injecting libnetty_tcnative_linux_.*.so would copy the file (from his jar) to a workdir
+	// then inject it to the process via System.Loadlibrary()
+	// Usually we got the event when the file is just created and contain 0 bytes
+	if strings.Contains(hostlibPath, "libnetty_tcnative_linux_") {
+		s, err := os.Stat(hostlibPath)
+		if err != nil {
+			return err
+		}
+		time.Sleep(time.Millisecond)
+		libSize := s.Size()
+		sizeOK := false
+		end := time.Now().Add(100 * time.Millisecond)
+		for !time.Now().After(end) {
+			s, err = os.Stat(hostlibPath)
+			if err != nil {
+				return err
+			}
+			if s.Size() != libSize || s.Size() == 0 {
+				libSize = s.Size()
+				time.Sleep(time.Millisecond)
+				continue
+			}
+			sizeOK = true
+			break
+		}
+		if !sizeOK {
+			return fmt.Errorf("timeout current size %d", libSize)
+		}
+	}
+	return nil
+}
+
 // Register a ELF library root/libPath as be used by the pid
 // Only one registration will be done per ELF (system wide)
 func (r *soRegistry) Register(root string, libPath string, pid uint32, rule soRule) {
@@ -315,6 +349,11 @@ func (r *soRegistry) Register(root string, libPath string, pid uint32, rule soRu
 	if reg, found := r.byID[pathID]; found {
 		reg.refcount++
 		r.byPID[pid] = reg
+		return
+	}
+
+	if err := quircksWaitLib(hostLibPath); err != nil {
+		log.Debugf("error registering library during waiting for lib %s path %s by pid %d : %s", pathID.String(), hostLibPath, pid, err)
 		return
 	}
 
